c33949c26010a034c8b5c13160e7dea4df8252e7
Aditya Parab <contact.adityaparab@gmail.com>
2022-07-12 15:00:08 +0200

    add user schema and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getMongoDbConnectionString } from './app.util';
+import { UsersModule } from './modules/user/user.module';
 
 console.log(getMongoDbConnectionString());
 
 @Module({
-  imports: [MongooseModule.forRoot(getMongoDbConnectionString())],
+  imports: [MongooseModule.forRoot(getMongoDbConnectionString()), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  createdAt: Date;
+
+  @Prop()
+  deletedAt?: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
